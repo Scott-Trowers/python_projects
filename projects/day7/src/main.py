9d5f804fdda06c0ef929b8f2fdd04eb21a417154
Scott Trowers <scott.trowers@atombank.co.uk>
2026-08-03 17:02:27 +0100

    minor: adds docstrings to functions

--- projects/day7/src/main.py
+++ projects/day7/src/main.py
@@ -2,6 +2,15 @@ import turtle as t
 import random
 
 def UI_setup(finish_line=None):
+    """
+    Sets up the screen background and draws the finish line marker.
+
+    Args:
+        finish_line (int, optional): The x-coordinate of the finish line. Defaults to screen width - 100.
+
+    Returns:
+        tuple: The Screen object, the announcer Turtle (splinter), and the finish line x-coordinate.
+    """
     screen = t.Screen()
     screen.bgcolor('lightblue')
 
@@ -31,6 +40,15 @@ def UI_setup(finish_line=None):
 
 
 def generate_turtles(turtle_names_and_colours: dict):
+    """
+    Creates and configures the racing turtles.
+
+    Args:
+        turtle_names_and_colours (dict): A dictionary mapping turtle names to their colors.
+
+    Returns:
+        dict: A dictionary mapping turtle names to their configured Turtle objects.
+    """
     turtles = {}
 
     for name, colour in turtle_names_and_colours.items():
@@ -42,6 +60,15 @@ def generate_turtles(turtle_names_and_colours: dict):
     return turtles
 
 def take_prediction(turtles):
+    """
+    Prompts the user to predict the winning turtle via a text input dialog.
+
+    Args:
+        turtles (dict): The dictionary of racing turtles.
+
+    Returns:
+        str: The name of the predicted winning turtle (lowercase).
+    """
     pred = ''
     turtle_names = turtles.keys()
     nice_turtle_names = ', '.join(turtle_names).title()
@@ -53,6 +80,13 @@ def take_prediction(turtles):
 
 
 def turtles_to_start_line(turtles, starting_y = -200):
+    """
+    Teleports all turtles to their starting positions on the left side of the screen.
+
+    Args:
+        turtles (dict): The dictionary of racing turtles.
+        starting_y (int, optional): The starting y-coordinate for the first turtle. Defaults to -200.
+    """
     starting_y = starting_y
 
     for name, object in turtles.items():
@@ -61,6 +95,13 @@ def turtles_to_start_line(turtles, starting_y = -200):
 
 
 def race_countdown(screen, splinter):
+    """
+    Displays a visual countdown (3, 2, 1, GO!) on the screen before the race starts.
+
+    Args:
+        screen: The Screen object.
+        splinter: The announcer Turtle object used to write text.
+    """
     for i in [3, 2, 1, 'GO!']:
         screen.delay(150)
         splinter.teleport(x=0, y=200)
@@ -69,11 +110,28 @@ def race_countdown(screen, splinter):
 
 
 def rand_move(turtle):
+    """
+    Moves a turtle forward by a random integer distance between 0 and 10.
+
+    Args:
+        turtle: The Turtle object to move.
+    """
     randint = random.randint(0, 10)
     turtle.forward(randint)
 
 
 def run_the_race(turtles, screen, finishing_line):
+    """
+    Executes the main race loop, moving turtles forward until all cross the finish line.
+
+    Args:
+        turtles (dict): The dictionary of racing turtles.
+        screen: The Screen object.
+        finishing_line (int): The x-coordinate of the finish line.
+
+    Returns:
+        dict: A dictionary mapping final positions (1st, 2nd, etc.) to turtle names.
+    """
     all_finished = False
     winners = {}
     turn = 0
@@ -100,16 +158,20 @@ def run_the_race(turtles, screen, finishing_line):
 
 
 def podium(winners_dict, turtles):
+    """
+    Positions the top three turtles on a winners' podium and hides the rest.
+
+    Args:
+        winners_dict (dict): A dictionary mapping positions to turtle names.
+        turtles (dict): The dictionary of racing turtles.
+    """
     first_place = turtles[winners_dict[1]]
     second_place = turtles[winners_dict[2]]
     third_place = turtles[winners_dict[3]]
 
     if len(winners_dict) > 3:
-        print('activated')
         for pos_num in range(4, len(winners_dict) + 1):
-            print(pos_num)
             loser_turtle = turtles[winners_dict[pos_num]]
-            print(loser_turtle)
             loser_turtle.teleport(1000, 1000)
 
     first_place.teleport(x=0, y=25)
@@ -118,6 +180,15 @@ def podium(winners_dict, turtles):
 
 
 def closing_process(splinter, screen, winners, pred):
+    """
+    Displays the winner of the race and whether the user's prediction was correct.
+
+    Args:
+        splinter: The announcer Turtle object used to write text.
+        screen: The Screen object.
+        winners (dict): A dictionary of race results.
+        pred (str): The user's predicted winner.
+    """
     correct_pred = (winners[1] == pred)
 
     screen.delay(150)
@@ -135,6 +206,12 @@ def closing_process(splinter, screen, winners, pred):
 
 
 def turtle_race(turtle_names_and_colours):
+    """
+    Orchestrates the entire turtle racing game.
+
+    Args:
+        turtle_names_and_colours (dict): A dictionary mapping turtle names to their colors.
+    """
     screen, splinter, finish_line = UI_setup()
 
     turtles = generate_turtles(turtle_names_and_colours)
