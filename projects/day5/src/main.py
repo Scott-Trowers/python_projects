e932e469d813b3f75bedc68162c3a7e8b675c9ea
Scott Trowers <scott.trowers@atombank.co.uk>
2026-08-07 14:21:38 +0100

    refactor: updates previous project folders to reflect contents

--- projects/day5/src/main.py
+++
@@ -1,5 +0,0 @@
-from projects.day5.src.quiz_brain import QuizBrain
-from projects.day5.src.data import question_data
-
-quiz_master = QuizBrain(question_data, question_key="question", answer_key="correct_answer")
-quiz_master.start_quiz()
