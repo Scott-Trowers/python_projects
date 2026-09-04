50599bad034e04713f0fcfb436c26d2609381928
Scott Trowers <scott.trowers@atombank.co.uk>
2026-09-04 11:44:52 +0100

    style: addresses line-too-long lint

--- projects/day13_nato_alphabet_converter/src/main.py
+++ projects/day13_nato_alphabet_converter/src/main.py
@@ -6,10 +6,9 @@ nato_dict = {row.letter:row.code for (index, row) in phon_alpha.iterrows()}
 
 user_input = input("Enter prompt: ").upper().split(' ')
 
-char_breakdown = [list(word) for word in user_input]
-print(char_breakdown)
+char_list = [list(word) for word in user_input]
 
-nato_output = [[nato_dict[letter] if letter in nato_dict.keys() else letter for letter in word] for word in char_breakdown]
+nato_output = [[nato_dict[letter] if letter in nato_dict.keys() else letter for letter in word] for word in char_list]
 
 for word in nato_output:
     print(word)
